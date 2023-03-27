bda181059ac9deac7aadcfa1937e53bb03dc4433
Tereza <tereza.chodurova@gmail.com>
2023-03-27 17:26:22 +0200

    Initial Commit

---
+++ Applifting task/Applifting_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Applifting_taskApp.swift
+//  Applifting task
+//
+//  Created by Terka on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Applifting_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Applifting task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Applifting task
+//
+//  Created by Terka on 27.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
